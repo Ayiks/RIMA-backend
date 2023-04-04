aac8615e87b86d8cd991d7288ce0124c9287e2c8
Kofi001 <t09ia22@abdn.ac.uk>
2023-04-04 15:39:21 +0000

    Added svae driver info route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const {databaseConnection} = require('./config/db')
-const authRoute = require('./v1/routes/authRoute')
-
+const {databaseConnection} = require('./config/db');
+const authRoute = require('./v1/routes/authRoute');
+const saveInfoRoute = require('./v1/routes/driverVehicleInfoRoute');
 const app = express();
 
 const PORT = 4001 || process.env.PORT;
@@ -10,6 +10,7 @@ databaseConnection();
 
 //calling of routes
 app.use('/rima/auth', authRoute);
+app.use('rima/', saveDriverInfo);
 
 
 
